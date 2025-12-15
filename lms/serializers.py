c51ed19d171b30a785433592e84b6fe30daf738d
Ксения <kssshaab@gmail.com>
2025-12-15 14:47:56 +0300

    add count lessons serializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,13 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from lms.models import Course, Lesson
+
+
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
 
 class CourseSerializer(ModelSerializer):
@@ -8,8 +15,16 @@ class CourseSerializer(ModelSerializer):
         model = Course
         fields = "__all__"
 
+class CourseDetailSerializer(ModelSerializer):
+    count_lessons = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
-class LessonSerializer(ModelSerializer):
     class Meta:
-        model = Lesson
-        fields = "__all__"
+        model = Course
+        fields = ("name", "count_lessons", "lessons")
+
+    def get_count_lessons(self, obj):
+        return obj.lessons.count()
+
+
+
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,9 @@ from rest_framework.routers import DefaultRouter
 
 from users.views import UserViewSet
 
+from users.apps import UsersConfig
+
+app_name = UsersConfig
 router = DefaultRouter()
 router.register("users", UserViewSet)
 
