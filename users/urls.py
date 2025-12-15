b4d95ef23566edf58291fe45ec365072acf15994
Ксения <kssshaab@gmail.com>
2025-12-15 19:02:09 +0300

    add filters

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentsViewSet
 
-app_name = UsersConfig
+app_name = "users"
 router = DefaultRouter()
 router.register("users", UserViewSet)
-
+router.register("payments", PaymentsViewSet)
 urlpatterns = router.urls
