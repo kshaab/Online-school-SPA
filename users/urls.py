dbb560ac6171fce3bd021810edbfaa16120cf63d
K <kssshaab@gmail.com>
2025-12-24 18:45:00 +0300

    settings

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,19 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
 from users.views import PaymentsViewSet, UserViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-app_name = "users"
+app_name = UsersConfig.name
 router = DefaultRouter()
 router.register("users", UserViewSet)
 router.register("payments", PaymentsViewSet)
-urlpatterns = router.urls
+
+urlpatterns = router.urls + [
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
